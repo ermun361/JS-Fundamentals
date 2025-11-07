91ca6327028c02496305a4fe14b6c47f6a9bf17e
Eric Munyi <munyieric7@gmail.com>
2025-11-07 15:41:06 +0300

    Add 2-arguments.js

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+const argv = process.argv.length - 2;
+
+if (argv === 0) {
+  console.log("No argument");
+} else if (argv === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
